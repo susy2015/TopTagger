3ecc15a432ce47c74e44926fae0f49f4f78a1741
Nathaniel <pastika@cern.ch>
2016-04-19 19:53:02 -0500

    Fix infinite loop bug

--- TopTagger/src/TopTagger.cpp
+++ TopTagger/src/TopTagger.cpp
@@ -14,41 +14,6 @@
 TopTagger::TopTagger()
 {
     topTaggerResults_ = nullptr;
-
-    //dummy configuration document
-    std::string cfgDocText = 
-        "scope\n"
-        "{\n"
-        "    var0 = \"Hello\"\n"
-        "    var1 = 45\n"
-        "    var2 = 37\n"
-        "}\n";
-
-    try
-    {
-        cfgDoc_ = cfg::CfgDocument::parseDocument(cfgDocText);
-
-        cfg::Context cxt("scope");
-
-        std::string var0 = cfgDoc_->get("var0", cxt, "Goodbye");
-
-        int var1 = cfgDoc_->get("var1", cxt, 123);
-        double var2 = cfgDoc_->get("var2", cxt, 321.123);
-        int var3 = cfgDoc_->get("var3", cxt, 646);
-
-        std::cout << "var0: " << var0 << std::endl;
-        std::cout << "var1: " << var1 << std::endl;
-        std::cout << "var2: " << var2 << std::endl;
-        std::cout << "var3: " << var3 << std::endl;
-    }
-    catch(const std::string e)
-    {
-        std::cout << e << std::endl;
-    }
-    catch(const char* e)
-    {
-        std::cout << e << std::endl;
-    }
 }
 
 TopTagger::TopTagger(const std::string& cfgFileName)
@@ -116,11 +81,13 @@ void TopTagger::getParameters()
 
     //Get list of modules to use
     int iModule = 0;
-    std::string moduleName;
+    bool keepLooping;
     do
     {
+        keepLooping = false;
+
         //Get module name
-        moduleName = cfgDoc_->get("module", iModule, cxt, "");
+        std::string moduleName = cfgDoc_->get("module", iModule, cxt, "");
 
         //if it is a non empty string look for module
         if(moduleName.size() > 0)
@@ -128,6 +95,9 @@ void TopTagger::getParameters()
             //Check in module map for this module
             if(TTMFactory::moduleExists(moduleName))
             {
+                //if a module is found, try again until one isn't
+                keepLooping = true;
+
                 //Create module and add to module to vector
                 topTaggerModules_.emplace_back(TTMFactory::createModule(moduleName));
             }
@@ -136,8 +106,9 @@ void TopTagger::getParameters()
                 throw "TopTagger::getParameters() : No module named \"" + moduleName + "\" exists"; 
             }
         }
+        ++iModule;
     }
-    while(moduleName.size());
+    while(keepLooping);
 }
 
 void TopTagger::registerModule(std::unique_ptr<TTModule>& module)
