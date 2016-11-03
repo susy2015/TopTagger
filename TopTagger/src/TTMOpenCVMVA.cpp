dcdd6f3b5dc911187938e601d8d2ee8029151c2e
Nathaniel <pastika@cern.ch>
2016-11-03 16:32:35 -0500

    Added check that candidates have 3 constituents

--- TopTagger/src/TTMOpenCVMVA.cpp
+++ TopTagger/src/TTMOpenCVMVA.cpp
@@ -31,23 +31,27 @@ void TTMOpenCVMVA::run(TopTaggerResults& ttResults)
 
     for(auto& topCand : topCandidates)
     {
-        //Construct data matrix for prediction
-        std::map<std::string, double> varMap = ttUtility::createMVAInputs(topCand);
-
-        cv::Mat inputData(vars_.size(), 1, 5); //the last 5 is for CV_32F var type
-        for(unsigned int i = 0; i < vars_.size(); ++i)
+        //We only want to apply the MVA algorithm to triplet tops
+        if(topCand.getNConstituents() == 3)
         {
-            inputData.at<float>(i, 0) = varMap[vars_[i]];
-        }
+            //Construct data matrix for prediction
+            std::map<std::string, double> varMap = ttUtility::createMVAInputs(topCand);
 
-        //predict value
-        double discriminator = treePtr_->predict(inputData);
-        topCand.setDiscriminator(discriminator);
+            cv::Mat inputData(vars_.size(), 1, 5); //the last 5 is for CV_32F var type
+            for(unsigned int i = 0; i < vars_.size(); ++i)
+            {
+                inputData.at<float>(i, 0) = varMap[vars_[i]];
+            }
 
-        //place in final top list if it passes the threshold
-        if(discriminator > discriminator_)
-        {
-            tops.push_back(&topCand);
+            //predict value
+            double discriminator = treePtr_->predict(inputData);
+            topCand.setDiscriminator(discriminator);
+
+            //place in final top list if it passes the threshold
+            if(discriminator > discriminator_)
+            {
+                tops.push_back(&topCand);
+            }
         }
     }
 }
