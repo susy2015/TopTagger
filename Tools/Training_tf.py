Modify translateData to add hierarchical data structure to encode event data, add new fake rate plots, modify event weighting and add event weighting by default for MLP, fix histogram binning on variable plots

Tools/Training_tf.py
@@ -41,10 +41,12 @@ def importData(prescale = True, reluInputs=True, ptReweight=True):
     data = pd.read_pickle(sample)
     #remove partial tops 
     inputLabels = data.as_matrix(["genConstiuentMatchesVec", "genTopMatchesVec"])
-    inputAnswer = (inputLabels[:,0] == 3) & inputLabels[:,1]
+    inputAnswer = (inputLabels[:,0] == 3) & (inputLabels[:,1] == 1)
     inputBackground = (inputLabels[:,0] == 0) & numpy.logical_not(inputLabels[:,1])
-    data = data[(inputAnswer == 1) | (inputBackground == 1)]
-    inputAnswer = inputAnswer[(inputAnswer == 1) | (inputBackground == 1)]
+    filterArray = ((inputAnswer == 1) | (inputBackground == 1)) & (data["ncand"] > 0)
+    data = data[filterArray]
+    inputAnswer = inputAnswer[filterArray]
+    inputWgts = numpy.copy(data.as_matrix(["sampleWgt"]).astype(numpy.float32))
     
     if ptReweight:
       #calculate pt weights
@@ -57,10 +59,8 @@ def importData(prescale = True, reluInputs=True, ptReweight=True):
       ptHistBg,  _ = numpy.histogram(dataPt[inputAnswer != 1], bins=ptBins, weights=inputSampleWgts[inputAnswer != 1])
       ptHistSig[ptHistSig < 10] = ptHistSig.max()
       ptHistBg[ptHistBg < 10] = ptHistBg.max()
-      inputWgts[inputAnswer == 1] = (1.0/ptHistSig[numpy.digitize(dataPt[inputAnswer == 1], ptBins) - 1]).reshape([-1,1])
-      inputWgts[inputAnswer != 1] = (1.0/ptHistBg [numpy.digitize(dataPt[inputAnswer != 1], ptBins) - 1]).reshape([-1,1])
-    else:
-      inputWgts = data.as_matrix(["sampleWgt"]).astype(numpy.float32)
+      inputWgts[inputAnswer == 1] *= (1.0/ptHistSig[numpy.digitize(dataPt[inputAnswer == 1], ptBins) - 1]).reshape([-1,1])
+      inputWgts[inputAnswer != 1] *= (1.0/ptHistBg [numpy.digitize(dataPt[inputAnswer != 1], ptBins) - 1]).reshape([-1,1])
 
     if len(inputData) == 0:
       inputData = data
@@ -72,7 +72,7 @@ def importData(prescale = True, reluInputs=True, ptReweight=True):
   #parse pandas dataframe into training data
   npyInputData = inputData.as_matrix(vars).astype(numpy.float32)
   npyInputLabels = inputData.as_matrix(["genConstiuentMatchesVec", "genTopMatchesVec"])
-  npyInputAnswer = (npyInputLabels[:,0] == 3) & npyInputLabels[:,1]
+  npyInputAnswer = (npyInputLabels[:,0] == 3) & (npyInputLabels[:,1] == 1)
   npyInputAnswers = numpy.vstack([npyInputAnswer,numpy.logical_not(npyInputAnswer)]).transpose()
   npyInputSampleWgts = inputData.as_matrix(["sampleWgt"]).astype(numpy.float32)
 
@@ -154,8 +154,8 @@ def mainTF(_):
   tf.add_to_collection('TrainInfo', x)
   tf.add_to_collection('TrainInfo', y)
 
-  #cross_entropy = tf.divide(tf.reduce_sum(tf.multiply(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=yt), wgt)), tf.reduce_sum(wgt))
-  cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=yt))
+  cross_entropy = tf.divide(tf.reduce_sum(tf.multiply(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=yt), wgt)), tf.reduce_sum(wgt))
+  #cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=yt))
   l2_norm = tf.constant(0.0)
   for w in w_fc.values():
     l2_norm += tf.nn.l2_loss(w)

Tools/translateData.py
@@ -16,16 +16,16 @@ def main():
     data = []
 
     branchNames = [b.GetName() for b in dataset.slimmedTuple.GetListOfBranches()]
-    print branchNames
+    #print branchNames
     evalStr = ""
     nevt  =0
-    rawName = ["eventNum"]
     for event in dataset.slimmedTuple:
         if nevt % 1000 == 0:
             print nevt
         nevt += 1
         if evalStr == "":
             bnList = []
+            bnListNoCand = []
             for bn in branchNames:
                 try:
                     nbranch = eval("len(dataset.slimmedTuple.%(bname)s)"%{"bname":bn})
@@ -35,18 +35,28 @@ def main():
                     continue
                 if nbranch < 0:
                     bnList.append("\"%(bname)s\":event.%(bname)s"%{"bname":bn})
+                    bnListNoCand.append("\"%(bname)s\":event.%(bname)s"%{"bname":bn})
                 elif nbranch >= 0:
                     bnList.append("\"%(bname)s\":event.%(bname)s[%%(i)i]"%{"bname":bn})
-            evalStr = "{\"eventNum\":%i, "%nevt + ",".join(bnList) + "}"
-            print evalStr
+                    bnListNoCand.append("\"%(bname)s\":0.0"%{"bname":bn})
+            evalStr       = "{\"eventNum\":%(nevt)i, \"candNum\":%(i)i, \"ncand\":%(ncand)i, " + ",".join(bnList) + "}"
+            evalStrNoCand = "{\"eventNum\":%(nevt)i, \"candNum\":%(i)i, \"ncand\":%(ncand)i, " + ",".join(bnListNoCand) + "}"
+            #print evalStr
         ncands = len(dataset.slimmedTuple.cand_m)
         for i in xrange(0, ncands):
-            data.append( eval(evalStr%{"i":i}) )
+            data.append( eval(evalStr%{"i":i, "nevt":nevt, "ncand":ncands}) )
+        if ncands == 0:
+            data.append( eval(evalStrNoCand%{"i":0, "nevt":nevt, "ncand":ncands}) )
 
     pdData = pd.DataFrame(data)
 
+    indices = [pdData.as_matrix(["eventNum"]).reshape([-1]), pdData.as_matrix(["candNum"]).reshape([-1])]
+    
+    fullBranchNames = np.hstack([["ncand"], branchNames] )
+    pdData = pd.DataFrame(pdData.as_matrix(fullBranchNames), index=pd.MultiIndex.from_arrays(indices), columns=fullBranchNames)
+
     #pdData.to_csv(datasetFile[0:-5] + ".csv")
-    pdData.to_pickle(datasetFile[0:-5] + ".pkl")
+    pdData.to_pickle(datasetFile[0:-5] + ".pkl.gz")
 
 if __name__ == '__main__':
     main()