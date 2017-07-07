ec237e490182d62e366ae66731a3fce0f264ce4d
Nathaniel <pastika@cern.ch>
2017-07-06 21:22:04 -0500

    Added TeamA style b-seeded clustering

--- TopTagger/src/TTMBasicClusterAlgo.cpp
+++ TopTagger/src/TTMBasicClusterAlgo.cpp
@@ -4,6 +4,8 @@
 #include "TopTagger/CfgParser/include/Context.hh"
 #include "TopTagger/CfgParser/include/CfgDocument.hh"
 
+#include <iostream>
+
 void TTMBasicClusterAlgo::getParameters(const cfg::CfgDocument* cfgDoc, const std::string& localContextName)
 {
     //Construct contexts
@@ -22,6 +24,7 @@ void TTMBasicClusterAlgo::getParameters(const cfg::CfgDocument* cfgDoc, const st
     maxTopCandMass_   = cfgDoc->get("maxTopCandMass",   localCxt, -999.9);
     doTrijet_         = cfgDoc->get("doTrijet",         localCxt,  false);
     dRMaxTrijet_      = cfgDoc->get("dRMaxTrijet",      localCxt, -999.9);
+    nbSeed_           = cfgDoc->get("nbSeed",           localCxt, -1);
 
     //get vars for TTMConstituentReqs
     TTMConstituentReqs::getParameters(cfgDoc, localContextName);
@@ -31,6 +34,16 @@ void TTMBasicClusterAlgo::run(TopTaggerResults& ttResults)
 {
     const std::vector< Constituent>& constituents = ttResults.getConstituents();
     std::vector<TopObject>& topCandidates = ttResults.getTopCandidates();
+
+    std::vector<const Constituent*> constituentsCSVSort;
+    if(nbSeed_ > 0)
+    {
+        for(const auto& constituent : constituents)
+        {
+            constituentsCSVSort.push_back(&constituent);
+        }
+        std::sort(constituentsCSVSort.begin(), constituentsCSVSort.end(), [](const Constituent* c1, const Constituent* c2) { return c1->getBTagDisc() > c2->getBTagDisc(); } );
+    }
 
     for(unsigned int i = 0; i < constituents.size(); ++i)
     {
@@ -81,26 +94,47 @@ void TTMBasicClusterAlgo::run(TopTaggerResults& ttResults)
                 for(unsigned int j = 0; j < i; ++j)
                 {
                     if(passAK4ResolvedReqs(constituents[j]))
-                    {                
-                        for(unsigned int k = 0; k < j; ++k)
+                    {
+                        if(nbSeed_ <= 0)
                         {
-                            if(passAK4ResolvedReqs(constituents[k]))
+                            for(unsigned int k = 0; k < j; ++k)
                             {
-                                TopObject topCand({&constituents[k], &constituents[j], &constituents[i]});
-
-                                //mass window on the top candidate mass
-                                double m123 = topCand.p().M();
-                                bool passMassWindow = (minTopCandMass_ < m123) && (m123 < maxTopCandMass_);
-
-                                if(topCand.getDRmax() < dRMaxTrijet_ && passMassWindow)
+                                if(passAK4ResolvedReqs(constituents[k]))
                                 {
-                                    topCandidates.push_back(topCand);
+                                    fillTriplet(&constituents[k], &constituents[j], &constituents[i], topCandidates);
                                 }
+                            }
+                        }
+                        else
+                        {
+                            for(int k = 0; k < std::min(nbSeed_, static_cast<int>(constituentsCSVSort.size())); ++k)
+                            {
+                                if(constituentsCSVSort[k] == &constituents[i] || constituentsCSVSort[k] == &constituents[j]) continue;
+
+                                if(passAK4ResolvedReqs(*constituentsCSVSort[k]))
+                                {
+                                    fillTriplet(constituentsCSVSort[k], &constituents[j], &constituents[i], topCandidates);
+                                }
+                                
                             }
                         }
                     }
                 }
             }
         }
+    }
+}
+
+void TTMBasicClusterAlgo::fillTriplet(const Constituent* const c1, const Constituent* const c2, const Constituent* const c3, std::vector<TopObject>& topCandidates)
+{
+    TopObject topCand({c1, c2, c3});
+
+    //mass window on the top candidate mass
+    double m123 = topCand.p().M();
+    bool passMassWindow = (minTopCandMass_ < m123) && (m123 < maxTopCandMass_);
+
+    if(topCand.getDRmax() < dRMaxTrijet_ && passMassWindow)
+    {
+        topCandidates.push_back(topCand);
     }
 }
