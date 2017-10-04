d9018097652f6ab5078a32d5d3d3f4ff4f71d5b0
Caleb James Smith <caleb_smith2@baylor.edu>
2017-10-04 14:24:28 -0500

    Add split plot functionality to plot diff pt cuts per file.

--- Tools/python/rocPlot.py
+++ Tools/python/rocPlot.py
@@ -6,123 +6,217 @@ import numpy as np
 import errno
 import os
 import optparse
+from datetime import datetime
 
-parser = optparse.OptionParser("usage: %prog [options]\n")
-parser.add_option('-d', "--directory",  dest='directory',   action='store',      default="",               help="Directory to store outputs")
-parser.add_option('-j', "--jsonFile",   dest='jsonFile',    action='store',      default="rocPlots.json",  help="Json file defining files and labels")
-parser.add_option('-s', "--split",      dest='split',       action='store_true', default=False,            help="Split ROC curves into separate plots per pt range.")
+class Plotter:
+    def __init__(self):
+        self.outputDirectory = ""
+        self.jsonFile = "rocPlots.json"
+        self.colors = ["red", "blue", "green", "orange", "black", "purple", "yellow", "pink", "maroon"]
 
-options, args = parser.parse_args()
 
-outputDirectory = ""
-
-if len(options.directory):
-  outputDirectory = options.directory
-  if outputDirectory[-1] != "/":
-      outputDirectory += "/"
-  try:
-      os.mkdir(outputDirectory)
-  except OSError as exc:
-      if exc.errno == errno.EEXIST and os.path.isdir(outputDirectory):
-          pass
-      else:
-          raise
-
-print "LOADING JSON FILE"
-
-jsonFile = options.jsonFile
-
-with open(jsonFile, "r") as f:
-    inputs = json.load(f)
-
-colors = ["red", "blue", "green", "orange", "black", "purple", "yellow", "pink", "maroon"]
-
-for name, filelist in inputs.iteritems():
-    plt.clf()
+def main():
+    parser = optparse.OptionParser("usage: %prog [options]\n")
+    parser.add_option('-d', "--directory",  dest='directory',   action='store',      default="",               help="Directory to store outputs")
+    parser.add_option('-j', "--jsonFile",   dest='jsonFile',    action='store',      default="rocPlots.json",  help="Json file defining files and labels")
+    parser.add_option('-s', "--split",      dest='split',       action='store_true', default=False,            help="Split ROC curves into separate plots per pt range.")
     
-    files = filelist["files"]
-    labels = filelist["labels"]
+    options, args = parser.parse_args()
 
-    plotRoc = plt.figure()
-    plotRocAx = plotRoc.add_subplot(111)
-    plotRocZ = plt.figure()
-    plotRocZAx = plotRocZ.add_subplot(111)
+    p = Plotter()
+    
+    if len(options.directory):
+      p.outputDirectory = options.directory
+      if p.outputDirectory[-1] != "/":
+          p.outputDirectory += "/"
+      try:
+          os.mkdir(p.outputDirectory)
+      except OSError as exc:
+          if exc.errno == errno.EEXIST and os.path.isdir(p.outputDirectory):
+              pass
+          else:
+              raise
+    
+    print "LOADING JSON FILE"
+    
+    p.jsonFile = options.jsonFile
+    
+    with open(p.jsonFile, "r") as f:
+        p.inputs = json.load(f)
 
-    rocs  = []
-    rocsZ = []
-    fileLines = []
-    cutLines = []
-    
-    ifile = 0
-    
-    # colors per file
-    for file1, label, color in zip(files, labels, colors):
-        print "OPENING PICKLE; NAME: {0} FILE: {1}".format(name, file1)
-        f1 = open(file1, "rb")
-        PtCutMap = pickle.load(f1) 
-        ncuts = len(PtCutMap)
-        styles = list( (k, ((k+1), (k+1)) ) for k in xrange(ncuts) ) 
-        if len(styles) > 0: styles[-1] = 'solid'
-        if len(styles) > 1: styles[-2] = 'dashed'
-        if len(styles) > 2: styles[-3] = 'dashdot'
-        if len(styles) > 3: styles[-4] = 'dotted'
+    split = options.split
+
+    if split:
+        makeSplitPlots(p) 
+    else:
+        makeCombinedPlots(p)
+
+def makeSplitPlots(plotter):
+    print "MAKING SPLIT PLOTS"
+    p = plotter
+    style = "solid"
+    for name, filelist in p.inputs.iteritems():
+        files = filelist["files"]
+        labels = filelist["labels"]
         
-        icut = 0
-        for cut in sorted(PtCutMap.iteritems()):
-            # styles per cut
-            style = styles[icut]
-            print "File: {0} Label: {1} Color: {2} Style: {3} Cut: {4}".format(file1, label, color, style, cut[0])
+        # plot p_t cuts per file
+        for file1, label in zip(files, labels):
+            print "OPENING PICKLE; NAME: {0} FILE: {1}".format(name, file1)
+            f1 = open(file1, "rb")
+            PtCutMap = pickle.load(f1) 
+            ncuts = len(PtCutMap)
 
-            PtCutData = PtCutMap[cut[0]]
-            TPRPtCut  = PtCutData["TPR"]  
-            FPRPtCut  = PtCutData["FPR"] 
-            FPRZPtCut = PtCutData["FPRZ"] 
-            pt_min = PtCutData["PtMin"]
-            pt_max = PtCutData["PtMax"]
-            
-            rocs.append(plotRocAx.plot(FPRPtCut,      TPRPtCut, label=label, linestyle=style, color=color, alpha=1.0)[0])
-            rocsZ.append(plotRocZAx.plot(FPRZPtCut,   TPRPtCut, label=label, linestyle=style, color=color, alpha=1.0)[0])
-            
-            if ifile == 0:
+            plotRoc  = plt.figure()
+            plotRocZ = plt.figure()
+            plotRocAx  = plotRoc.add_subplot(111)
+            plotRocZAx = plotRocZ.add_subplot(111)
+            rocs  = []
+            rocsZ = []
+
+            icut = 0
+            for cut in sorted(PtCutMap.iteritems()):
+                color = p.colors[icut]
+                print "File: {0} Label: {1} Color: {2} Cut: {3}".format(file1, label, color, cut[0])
+                PtCutData = PtCutMap[cut[0]]
+                TPRPtCut  = PtCutData["TPR"]  
+                FPRPtCut  = PtCutData["FPR"] 
+                FPRZPtCut = PtCutData["FPRZ"] 
+                pt_min = PtCutData["PtMin"]
+                pt_max = PtCutData["PtMax"]
+                
                 lineLabel = ""
                 if pt_max > 0:
                     lineLabel = r'${0}$ GeV $< p_T < {1}$ GeV'.format(pt_min, pt_max)
                 else:
                     lineLabel = r'$p_T > {0}$ GeV'.format(pt_min)
-                cutLines.append(mlines.Line2D([], [], color='black', marker=None, linestyle=style, label=lineLabel))
-
-            icut += 1
-
-        fileLines.append(mlines.Line2D([], [], color=color, marker=None, linestyle='solid', label=label))
-        ifile += 1
-
-    first_legend = plotRocAx.legend(handles=fileLines, loc="lower right")
-    plotRoc.gca().add_artist(first_legend)
-    second_legend = plotRocAx.legend(handles=cutLines, loc=(0.2, 0.02))
+                
+                rocs.append(plotRocAx.plot(FPRPtCut,      TPRPtCut, label=lineLabel, linestyle=style, color=color, alpha=1.0)[0])
+                rocsZ.append(plotRocZAx.plot(FPRZPtCut,   TPRPtCut, label=lineLabel, linestyle=style, color=color, alpha=1.0)[0])
+                icut += 1
+        
+            first_legend = plotRocAx.legend(handles=rocs, loc="lower right")
+            plotRoc.gca().add_artist(first_legend)
+            
+            plotRocAx.set_xlabel("FPR (ttbar)")
+            plotRocAx.set_ylabel("TPR (ttbar)")
+            plotRocAx.set_title("ROC Plot for TPR (ttbar) vs FPR (ttbar) "+label)
+            plotRocAx.set_xlim(0.0, 0.5)
+            plotRocAx.set_ylim(0.2, 1.0)
+            plotRoc.savefig("{0}roc_{1}_{2}.png".format(p.outputDirectory, name, label))
+            plotRoc.savefig("{0}roc_{1}_{2}.pdf".format(p.outputDirectory, name, label))
+            plt.close(plotRoc)
     
-    #plotRocAx.legend(loc="upper left")
-    plotRocAx.set_xlabel("FPR (ttbar)")
-    plotRocAx.set_ylabel("TPR (ttbar)")
-    plotRocAx.set_title("ROC Plot for TPR (ttbar) vs FPR (ttbar)")
-    plotRocAx.set_xlim(0.0, 0.5)
-    plotRocAx.set_ylim(0.2, 1.0)
-    #plotRocAx.xscale('log')
-    #plotRocAx.yscale('log')
-    plotRoc.savefig("{0}roc_{1}.png".format(outputDirectory, name))
-    plotRoc.savefig("{0}roc_{1}.pdf".format(outputDirectory, name))
-    plt.close(plotRoc)
-
-    first_legend = plotRocZAx.legend(handles=fileLines, loc="lower right")
-    plotRocZ.gca().add_artist(first_legend)
-    second_legend = plotRocZAx.legend(handles=cutLines, loc=(0.2, 0.02))
-
-    #plotRocZAx.legend(loc="lower right")
-    plotRocZAx.set_xlabel("FPR (Znunu)")
-    plotRocZAx.set_ylabel("TPR (ttbar)")
-    plotRocZAx.set_title("ROC Plot for TPR (ttbar) vs FPR (Znunu)")
-    plotRocZAx.set_xlim(0.0, 0.5)
-    plotRocZAx.set_ylim(0.2, 1.0)
-    plotRocZ.savefig("{0}rocZ_{1}.png".format(outputDirectory, name))
-    plotRocZ.savefig("{0}rocZ_{1}.pdf".format(outputDirectory, name))
-    plt.close(plotRocZ)
+            first_legend = plotRocZAx.legend(handles=rocsZ, loc="lower right")
+            plotRocZ.gca().add_artist(first_legend)
     
+            plotRocZAx.set_xlabel("FPR (Znunu)")
+            plotRocZAx.set_ylabel("TPR (ttbar)")
+            plotRocZAx.set_title("ROC Plot for TPR (ttbar) vs FPR (Znunu) "+label)
+            plotRocZAx.set_xlim(0.0, 0.5)
+            plotRocZAx.set_ylim(0.2, 1.0)
+            plotRocZ.savefig("{0}rocZ_{1}_{2}.png".format(p.outputDirectory, name, label))
+            plotRocZ.savefig("{0}rocZ_{1}_{2}.pdf".format(p.outputDirectory, name, label))
+            plt.close(plotRocZ)
+
+def makeCombinedPlots(plotter):
+    print "MAKING COMBINED PLOTS"
+    p = plotter
+    iname = 0
+    for name, filelist in p.inputs.iteritems():
+        print "BEGIN plt.clf()"
+        t_a = datetime.now()
+        plt.clf()
+        t_b = datetime.now()
+        print "END plt.clf()"
+        print "Time for plt.clf(): {0}".format(t_b - t_a)
+        
+        files = filelist["files"]
+        labels = filelist["labels"]
+        
+        plotRoc  = plt.figure()
+        plotRocZ = plt.figure()
+        plotRocAx  = plotRoc.add_subplot(111)
+        plotRocZAx = plotRocZ.add_subplot(111)
+    
+        rocs  = []
+        rocsZ = []
+        fileLines = []
+        cutLines = []
+        
+        ifile = 0
+        
+        # colors per file
+        for file1, label, color in zip(files, labels, p.colors):
+            print "OPENING PICKLE; NAME: {0} FILE: {1}".format(name, file1)
+            f1 = open(file1, "rb")
+            PtCutMap = pickle.load(f1) 
+            ncuts = len(PtCutMap)
+            styles = list( (k, ((k+1), (k+1)) ) for k in xrange(ncuts) ) 
+            if len(styles) > 0: styles[-1] = 'solid'
+            if len(styles) > 1: styles[-2] = 'dashed'
+            if len(styles) > 2: styles[-3] = 'dashdot'
+            if len(styles) > 3: styles[-4] = 'dotted'
+            
+            icut = 0
+            for cut in sorted(PtCutMap.iteritems()):
+                # styles per cut
+                style = styles[icut]
+                print "File: {0} Label: {1} Color: {2} Style: {3} Cut: {4}".format(file1, label, color, style, cut[0])
+    
+                PtCutData = PtCutMap[cut[0]]
+                TPRPtCut  = PtCutData["TPR"]  
+                FPRPtCut  = PtCutData["FPR"] 
+                FPRZPtCut = PtCutData["FPRZ"] 
+                pt_min = PtCutData["PtMin"]
+                pt_max = PtCutData["PtMax"]
+                
+                rocs.append(plotRocAx.plot(FPRPtCut,      TPRPtCut, label=label, linestyle=style, color=color, alpha=1.0)[0])
+                rocsZ.append(plotRocZAx.plot(FPRZPtCut,   TPRPtCut, label=label, linestyle=style, color=color, alpha=1.0)[0])
+                
+                if ifile == 0:
+                    lineLabel = ""
+                    if pt_max > 0:
+                        lineLabel = r'${0}$ GeV $< p_T < {1}$ GeV'.format(pt_min, pt_max)
+                    else:
+                        lineLabel = r'$p_T > {0}$ GeV'.format(pt_min)
+                    cutLines.append(mlines.Line2D([], [], color='black', marker=None, linestyle=style, label=lineLabel))
+    
+                icut += 1
+    
+            fileLines.append(mlines.Line2D([], [], color=color, marker=None, linestyle='solid', label=label))
+            ifile += 1
+    
+        first_legend = plotRocAx.legend(handles=fileLines, loc="lower right")
+        plotRoc.gca().add_artist(first_legend)
+        second_legend = plotRocAx.legend(handles=cutLines, loc=(0.2, 0.02))
+        
+        #plotRocAx.legend(loc="upper left")
+        plotRocAx.set_xlabel("FPR (ttbar)")
+        plotRocAx.set_ylabel("TPR (ttbar)")
+        plotRocAx.set_title("ROC Plot for TPR (ttbar) vs FPR (ttbar)")
+        plotRocAx.set_xlim(0.0, 0.5)
+        plotRocAx.set_ylim(0.2, 1.0)
+        #plotRocAx.xscale('log')
+        #plotRocAx.yscale('log')
+        plotRoc.savefig("{0}roc_{1}.png".format(p.outputDirectory, name))
+        plotRoc.savefig("{0}roc_{1}.pdf".format(p.outputDirectory, name))
+        plt.close(plotRoc)
+    
+        first_legend = plotRocZAx.legend(handles=fileLines, loc="lower right")
+        plotRocZ.gca().add_artist(first_legend)
+        second_legend = plotRocZAx.legend(handles=cutLines, loc=(0.2, 0.02))
+    
+        #plotRocZAx.legend(loc="lower right")
+        plotRocZAx.set_xlabel("FPR (Znunu)")
+        plotRocZAx.set_ylabel("TPR (ttbar)")
+        plotRocZAx.set_title("ROC Plot for TPR (ttbar) vs FPR (Znunu)")
+        plotRocZAx.set_xlim(0.0, 0.5)
+        plotRocZAx.set_ylim(0.2, 1.0)
+        plotRocZ.savefig("{0}rocZ_{1}.png".format(p.outputDirectory, name))
+        plotRocZ.savefig("{0}rocZ_{1}.pdf".format(p.outputDirectory, name))
+        plt.close(plotRocZ)
+        
+        iname += 1
+        
+if __name__ == "__main__":
+    main()
