80746dae1b825cf5eb039a487e00f76c59900a00
Nathaniel <pastika@cern.ch>
2017-12-21 18:00:39 -0600

    Switch to using docker

---
+++ .travis/runTests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo $TRAVIS_BUILD_DIR
+cd $TRAVIS_BUILD_DIR
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch                                        
+export SCRAM_ARCH=slc6_amd64_gcc493                                            
+export CMSSW_GIT_REFERENCE=/cvmfs/cms.cern.ch/cmssw.git/                       
+source $VO_CMS_SW_DIR/cmsset_default.sh 
+scramv1 project CMSSW CMSSW_9_3_3
+cd CMSSW_9_3_3/src/
+eval `scramv1 runtime -sh`
+cd $TRAVIS_BUILD_DIR/TopTagger/test
+which g++
+echo "int main() {}" > test.cpp
+g++ test.cpp
+./configure
+make -j
+source taggerSetup.sh
+$TRAVIS_BUILD_DIR/TopTagger/Tools/getTaggerCfg.sh -t Intermediate_Example_v1.0.0
+./topTaggerTest
