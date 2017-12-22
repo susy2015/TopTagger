c1c9b0db30b2a3d4e73298220c207e00d196aaaf
Nathaniel <pastika@cern.ch>
2017-12-21 22:32:55 -0600

    Fix cd

--- .travis/runTests.sh
+++ .travis/runTests.sh
@@ -2,7 +2,6 @@
 
 TRAVIS_BUILD_DIR=$1
 echo $TRAVIS_BUILD_DIR
-ls $TRAVIS_BUILD_DIR
 cd $TRAVIS_BUILD_DIR/..
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch                                        
 export SCRAM_ARCH=slc6_amd64_gcc630
@@ -10,12 +9,9 @@ export CMSSW_GIT_REFERENCE=/cvmfs/cms.cern.ch/cmssw.git/
 source $VO_CMS_SW_DIR/cmsset_default.sh 
 scramv1 project CMSSW CMSSW_9_3_3
 cd CMSSW_9_3_3/src/
-pwd
 eval `scramv1 runtime -sh`
 cp -r $TRAVIS_BUILD_DIR .
-cd TopTagger/test
-scram tool info py2-numpy
-scram tool tag py2-numpy PY2_NUMPY_BASE
+cd TopTagger/TopTagger/test
 ./configure
 make -j
 source taggerSetup.sh
