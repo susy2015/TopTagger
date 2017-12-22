bea8c1ecfeb0a9b6dfdc2213ac9f3533d5089fe3
Nathaniel <pastika@cern.ch>
2017-12-21 22:00:52 -0600

    Perform build in CMSSW release

--- .travis/runTests.sh
+++ .travis/runTests.sh
@@ -2,7 +2,7 @@
 
 TRAVIS_BUILD_DIR=$1
 echo $TRAVIS_BUILD_DIR
-cd $TRAVIS_BUILD_DIR
+cd $TRAVIS_BUILD_DIR/..
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch                                        
 export SCRAM_ARCH=slc6_amd64_gcc630
 export CMSSW_GIT_REFERENCE=/cvmfs/cms.cern.ch/cmssw.git/                       
@@ -11,7 +11,8 @@ scramv1 project CMSSW CMSSW_9_3_3
 cd CMSSW_9_3_3/src/
 pwd
 eval `scramv1 runtime -sh`
-cd $TRAVIS_BUILD_DIR/TopTagger/test
+mv $TRAVIS_BUILD_DIR .
+cd TopTagger/test
 scram tool info py2-numpy
 scram tool tag py2-numpy PY2_NUMPY_BASE
 ./configure
@@ -19,3 +20,5 @@ make -j
 source taggerSetup.sh
 $TRAVIS_BUILD_DIR/TopTagger/Tools/getTaggerCfg.sh -t Intermediate_Example_v1.0.0
 ./topTaggerTest
+cd ../..
+scram b -j
