87332ea919a4b8e5a3f001e335c89bad5e609f76
Travis CI <travis@travis-ci.org>
2018-04-18 21:26:53 +0000

    Deploy code docs to GitHub Pages Travis build: 85
    Commit: c8830b51d796d8d15eceeee8d130e4d6718aaf07

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -2,6 +2,9 @@ var searchData=
 [
   ['data_5fiterator',['data_iterator',['../classCustomQueueRunner_1_1CustomQueueRunner.html#aa15d0ce6375f74387b8b85f28356bd8c',1,'CustomQueueRunner::CustomQueueRunner']]],
   ['datagetter',['DataGetter',['../classDataGetter_1_1DataGetter.html',1,'DataGetter']]],
+  ['datamanager',['DataManager',['../classDataManager_1_1DataManager.html',1,'DataManager']]],
+  ['datasample',['DataSample',['../classDataSample_1_1DataSample.html',1,'DataSample']]],
+  ['dataset',['DataSet',['../classDataSet_1_1DataSet.html',1,'DataSet']]],
   ['destroyexiststype',['DestroyExistsType',['../structhcalcfg_1_1ParserDestroyExistsRecognizer_1_1DestroyExistsType.html',1,'hcalcfg::ParserDestroyExistsRecognizer']]],
   ['destroyismissingtype',['DestroyIsMissingType',['../structhcalcfg_1_1ParserDestroyExistsRecognizer_1_1DestroyIsMissingType.html',1,'hcalcfg::ParserDestroyExistsRecognizer']]]
 ];
--- html/search/classes_3.js
+++ html/search/classes_3.js
@@ -1,6 +1,9 @@
 var searchData=
 [
   ['datagetter',['DataGetter',['../classDataGetter_1_1DataGetter.html',1,'DataGetter']]],
+  ['datamanager',['DataManager',['../classDataManager_1_1DataManager.html',1,'DataManager']]],
+  ['datasample',['DataSample',['../classDataSample_1_1DataSample.html',1,'DataSample']]],
+  ['dataset',['DataSet',['../classDataSet_1_1DataSet.html',1,'DataSet']]],
   ['destroyexiststype',['DestroyExistsType',['../structhcalcfg_1_1ParserDestroyExistsRecognizer_1_1DestroyExistsType.html',1,'hcalcfg::ParserDestroyExistsRecognizer']]],
   ['destroyismissingtype',['DestroyIsMissingType',['../structhcalcfg_1_1ParserDestroyExistsRecognizer_1_1DestroyIsMissingType.html',1,'hcalcfg::ParserDestroyExistsRecognizer']]]
 ];
