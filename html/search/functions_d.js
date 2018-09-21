2262c189b75087fbce006c446092d3d8bdcc98d0
Travis CI <travis@travis-ci.org>
2018-09-21 20:46:31 +0000

    Deploy code docs to GitHub Pages Travis build: 162
    Commit: 483da4d28afa13c5d19fb9ae37ba7fcc9d8958ce

--- html/search/functions_d.js
+++ html/search/functions_d.js
@@ -3,7 +3,7 @@ var searchData=
   ['set',['set',['../classcfg_1_1Context.html#af1ab6523fda2781a6d7d3316fea68fe8',1,'cfg::Context::set(const std::string &amp;name, const std::string &amp;value)'],['../classcfg_1_1Context.html#a983796d226d2d2deab971f354b2ac649',1,'cfg::Context::set(const std::string &amp;name, const char *value)'],['../classcfg_1_1Context.html#a06edab7e54985b14fea0e088859f2c4b',1,'cfg::Context::set(const std::string &amp;name, int value)'],['../classcfg_1_1Context.html#a3dbae42248afdcc80c10cccf79f7ed88',1,'cfg::Context::set(const std::string &amp;name, bool value)']]],
   ['setcfgfile',['setCfgFile',['../classTopTagger.html#ac18bfe0673dbd05973f45c668ed2b190',1,'TopTagger']]],
   ['setcfgfiledirect',['setCfgFileDirect',['../classTopTagger.html#ae7a38be12023643495adea6f9a696ff3',1,'TopTagger']]],
-  ['setconstituents',['setConstituents',['../classTopTaggerResults.html#a2d7cf77c1e4591e6fbdf0b297a3f87bd',1,'TopTaggerResults']]],
+  ['setconstituents',['setConstituents',['../classTopTaggerResults.html#a2d7cf77c1e4591e6fbdf0b297a3f87bd',1,'TopTaggerResults::setConstituents(const std::vector&lt; Constituent &gt; &amp;constituents)'],['../classTopTaggerResults.html#a4ab733379f6e450ce98b2afdb4f6e523',1,'TopTaggerResults::setConstituents(std::vector&lt; Constituent &gt; &amp;&amp;constituents)']]],
   ['setdiscriminator',['setDiscriminator',['../classTopObject.html#abf5123e8c707e9059c8ec96b35643bef',1,'TopObject']]],
   ['setextravar',['setExtraVar',['../classConstituent.html#a1b2fc086bc359e141e54ed2903c951c9',1,'Constituent']]],
   ['setptr',['setPtr',['../classttUtility_1_1MVAInputCalculator.html#afae8a686f0fd4310a75c79216a39fa53',1,'ttUtility::MVAInputCalculator']]],
