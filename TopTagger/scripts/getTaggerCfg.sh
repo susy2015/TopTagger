97a3cdae14d728e0aa4f78983978f38549ae84f3
caleb-james-smith <caleb_smith2@baylor.edu>
2018-10-12 18:48:06 -0500

    Add VERBOSE flag... and fix bug (use 1 and not VERBOSE in the if... if you know what I mean).

--- TopTagger/scripts/getTaggerCfg.sh
+++ TopTagger/scripts/getTaggerCfg.sh
@@ -2,24 +2,29 @@
 
 # getTaggerCfg.sh
 
+# Required to run this script:
+# - release tag from TopTaggerCfg repo that you wish to checkout
+
 GITHUB_SUSY2015_URL=https://github.com/susy2015
 REPO_NAME=TopTaggerCfg
+RELEASE_URL="$GITHUB_SUSY2015_URL/$REPO_NAME/releases"
 
 STARTING_DIR=$PWD
 CFG_DIRECTORY=$PWD
 TAG=
 NO_SOFTLINK=
 OVERWRITE=
+VERBOSE=
 
 # A POSIX variable
-OPTIND=1         # Reset in case getopts has been used previously in the shell.
+OPTIND=1    # Reset in case getopts has been used previously in the shell.
 
 TOP_CFG_NAME=TopTagger.cfg
 
 function print_help {
     echo ""
     echo "Usage:"
-    echo "    getTaggerCfg.sh -t RELEASE_TAG [-d checkout_directory] [-f cfg_filename] [-n]"
+    echo "    getTaggerCfg.sh -t RELEASE_TAG [-d checkout_directory] [-f cfg_filename] [-o] [-n] [-v]"
     echo ""
     echo "Options:"
     echo "    -t RELEASE_TAG :         This is the github release tag to check out (required option)"
@@ -27,19 +32,20 @@ function print_help {
     echo "    -f cfg_filename :        Specify this option to name the softlink to the cfg file something other than \"TopTagger.cfg\""
     echo "    -o :                     Overwrite the softlinks if they already exist"
     echo "    -n :                     Download files without producing softlinks"
+    echo "    -v :                     increase verbosity: print more stuff... for those who like stuff"
     echo ""
     echo "Description:"
-    echo "    This script automatically downloads the top tagger configuration file and MVA training file (if necessary)"
+    echo "    This script automatically downloads the top tagger configuration file and MVA training files (if necessary)"
     echo "    and produces a softlink to this file in your corrent directory.  This script should be run from the directory where"
     echo "    the tagger code will be run from.  Tagger configuration releases can be browsed at"
-    echo "    https://github.com/susy2015/TopTaggerCfg/releases."
+    echo "    $RELEASE_URL"
     echo ""
 }
 
 
 # Initialize our own variables:
 
-while getopts "h?d:f:t:no" opt; do
+while getopts "h?d:f:t:nov" opt; do
     case "$opt" in
     h|\?)
         print_help
@@ -54,6 +60,8 @@ while getopts "h?d:f:t:no" opt; do
     o) OVERWRITE="-f"
         ;;
     n) NO_SOFTLINK=NO
+        ;;
+    v) VERBOSE=1
         ;;
     esac
 done
@@ -116,24 +124,36 @@ cd $CFG_DIRECTORY
 
 if [ ! -d $REPO_NAME-$TAG ]
 then
-    echo "Checking out tag: " $TAG
-    wget $GITHUB_SUSY2015_URL/$REPO_NAME/archive/$TAG.tar.gz
+    echo " - Downloading this REPO-TAG: $REPO_NAME-$TAG"
+    if [[ ! -z $VERBOSE ]] # True if VERBOSE is set
+    then
+        wget $GITHUB_SUSY2015_URL/$REPO_NAME/archive/$TAG.tar.gz
+    else
+        wget $GITHUB_SUSY2015_URL/$REPO_NAME/archive/$TAG.tar.gz &> /dev/null
+    fi
     if [ -f $TAG.tar.gz ]
     then
         tar xzf $TAG.tar.gz
         rm $TAG.tar.gz
     else
-        echo "Failed to get " $TAG.tar.gz
-        exit 0
+        echo "ERROR: Failed to download $GITHUB_SUSY2015_URL/$REPO_NAME/archive/$TAG.tar.gz"
+        echo "  Check that the REPO-TAG that you entered ($REPO_NAME-$TAG)"
+        echo "  exists at $RELEASE_URL"
+        echo "  Check your spelling... you may have a typo! Copy and paste are your friends."
+        exit 1
     fi
 else
-    echo "Directory "$REPO_NAME-$TAG" already present"
+    echo " - Skipping the download of the requested REPO-TAG because the directory "$REPO_NAME-$TAG" is already present"
 fi
+
 
 cd $REPO_NAME-$TAG
 DOWNLOAD_DIR=$PWD
 
-echo "INFO: DOWNLOAD_DIR is $DOWNLOAD_DIR"
+if [[ ! -z $VERBOSE ]] # True if VERBOSE is set
+then
+    echo "INFO: DOWNLOAD_DIR is $DOWNLOAD_DIR"
+fi
 
 MVAFILES=
 
@@ -152,18 +172,29 @@ then
         done
         if [[ ! -z ${MISSING// } ]]
         then
+            echo " - Downloading MVA files"
             MVATARBALL=MVAFILES.tar.gz
-            wget $GITHUB_SUSY2015_URL/$REPO_NAME/releases/download/$TAG/$MVATARBALL
+            if [[ ! -z $VERBOSE ]] # True if VERBOSE is set
+            then
+                wget $GITHUB_SUSY2015_URL/$REPO_NAME/releases/download/$TAG/$MVATARBALL
+            else
+                wget $GITHUB_SUSY2015_URL/$REPO_NAME/releases/download/$TAG/$MVATARBALL &> /dev/null
+            fi
             if [ ! -f $MVATARBALL ]
             then
-                echo "MVA tarball "$MVATARBALL" not found!!!"
+                echo "ERROR: MVA tarball "$MVATARBALL" not found!!!"
                 MVATARBALL=${MVAFILES%.*}.tar.gz
-                echo "trying "$MVATARBALL
-                wget $GITHUB_SUSY2015_URL/$REPO_NAME/releases/download/$TAG/$MVATARBALL
+                echo "Now trying $MVATARBALL instead"
+                if [[ ! -z $VERBOSE ]] # True if VERBOSE is set
+                then
+                    wget $GITHUB_SUSY2015_URL/$REPO_NAME/releases/download/$TAG/$MVATARBALL
+                else
+                    wget $GITHUB_SUSY2015_URL/$REPO_NAME/releases/download/$TAG/$MVATARBALL &> /dev/null
+                fi
                 if [ ! -f $MVATARBALL ]
                 then
-                    echo "MVA tarball "$MVATARBALL" not found!!!"
-                    exit 0
+                    echo "ERROR: MVA tarball "$MVATARBALL" not found!!!"
+                    exit 1
                 fi
             fi
             tar xzf $MVATARBALL
@@ -174,7 +205,10 @@ fi
 
 cd $STARTING_DIR
 
-echo "INFO: STARTING_DIR is $STARTING_DIR"
+if [[ ! -z $VERBOSE ]] # True if VERBOSE is set
+then
+    echo "INFO: STARTING_DIR is $STARTING_DIR"
+fi
 
 # If OVERWRITE is set, make solftlinks (using ln) with -f
 # If OVERWRITE is not set, make solftlinks (using ln)
@@ -188,9 +222,12 @@ echo "INFO: STARTING_DIR is $STARTING_DIR"
 # [[ -z STRING ]] : True if the length of "STRING" is zero, False if "STRING" has nonzero length
 if [[ -z $NO_SOFTLINK ]]
 then
+    # create softlinks
+    echo " - Creating softlinks to $REPO_NAME config file"
     ln $OVERWRITE -s $DOWNLOAD_DIR/TopTagger.cfg $TOP_CFG_NAME > /dev/null 2>&1
     if [[ ! -z ${MVAFILES// } ]] 
     then
+        echo " - Creating softlinks to $REPO_NAME MVA files"
         for MVAFILE in $MVAFILES; do
             ln $OVERWRITE -s $DOWNLOAD_DIR/$MVAFILE $MVAFILE > /dev/null 2>&1
         done
