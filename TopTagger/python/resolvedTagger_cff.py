aec2a406c2fde071f979416d55df7bb23470499d
Nathaniel <pastika@cern.ch>
2018-11-20 20:04:52 -0600

    Add nanoAOD customizer cff to add tagger results to nanoAOD

---
+++ TopTagger/python/resolvedTagger_cff.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+from  PhysicsTools.NanoAOD.common_cff import *
+
+#Kevin's function of magic #1
+def addJetInfo(process, JetTag, userFloats=[], userInts=[], btagDiscrs=cms.VInputTag(), suff=""):
+    # add userfloats to jet collection
+    from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJets as patJetsUpdated
+
+    # default suffix
+    if len(suff)==0: suff = "Auxiliary"
+    
+    JetTagOut = cms.InputTag(JetTag.value()+suff)
+    patJetsAuxiliary = patJetsUpdated.clone(
+        jetSource = JetTag,
+        addJetCorrFactors = cms.bool(False),
+        addBTagInfo = cms.bool(False)
+    )
+    patJetsAuxiliary.userData.userFloats.src += userFloats
+    patJetsAuxiliary.userData.userInts.src += userInts
+    if len(btagDiscrs)>0:
+        patJetsAuxiliary.discriminatorSources = btagDiscrs
+        patJetsAuxiliary.addBTagInfo = cms.bool(True)
+    setattr(process,JetTagOut.value(),patJetsAuxiliary)
+    
+    return (process, JetTagOut)
+
+
+def customizeResolvedTagger(process):
+    process.load('RecoJets.JetProducers.QGTagger_cfi')
+    process.QGTagger.srcJets   = cms.InputTag("slimmedJets")
+    process.QGTagger.jetsLabel = cms.string('QGL_AK4PFchs')
+    
+    process, jetTag = addJetInfo(process, cms.InputTag("slimmedJets"), userFloats=['QGTagger:qgLikelihood','QGTagger:ptD', 'QGTagger:axis1', 'QGTagger:axis2'], userInts=['QGTagger:mult'], suff="")
+    
+    process.load("TopTagger.TopTagger.SHOTProducer_cfi")
+    process.SHOTProducer.ak4JetSrc = jetTag
+    
+    process.resolvedTopTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+        src=cms.InputTag("SHOTProducer"),
+        cut=cms.string(""),
+        name=cms.string("ResolvedTop"),
+        doc=cms.string("Neural network top tagger results (http://susy2015.github.io/TopTagger)."),
+        singleton=cms.bool(False),
+        extension=cms.bool(False),
+        variables=cms.PSet(P4Vars,
+                           discriminator = Var("getDiscriminator()", float, doc="top discriminator"),
+                           type = Var("getType()", int, doc="top type (http://susy2015.github.io/TopTagger/html/classTopObject.html, \"enum Type\")"),
+                           j1Idx = Var("getJ1Idx()", int, doc="index to the first jet in the top"),
+                           j2Idx = Var("getJ2Idx()", int, doc="index to the second jet in the top"),
+                           j3Idx = Var("getJ3Idx()", int, doc="index to the third jet in the top"),
+        )
+    )
+
+    process.resolvedTask = cms.Task(process.QGTagger, process.slimmedJetsAuxiliary, process.SHOTProducer, process.resolvedTopTable)
+
+    path = None
+    if path is None:
+        process.schedule.associate(process.resolvedTask)
+    else:
+        getattr(process, path).associate(process.resolvedTask)    
+
+    return process
