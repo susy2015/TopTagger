5bc63341c7bcef8a3658007d05fffce56a95b132
Nathaniel <pastika@cern.ch>
2018-11-20 20:03:37 -0600

    TopObjLite no inherits from reco::Candidate so it can be saved to nanoAOD

--- TopTagger/include/TopObjLite.h
+++ TopTagger/include/TopObjLite.h
@@ -1,22 +1,38 @@
+#include "DataFormats/Candidate/interface/LeafCandidate.h"
+
 #include "TopTagger/TopTagger/include/TopObject.h"
 
 #include "TLorentzVector.h"
 
-class TopObjLite
+/**
+ * TopObjLite is a version of top object which is derived from cmssw reco::Candidate (through reco::LeafCandidate) to allow the critical information to be saved into nanoAOD data format
+ */
+class TopObjLite : public reco::LeafCandidate
 {
-public:
-    TLorentzVector p4;
-    float discriminator;
-    int type;
-    unsigned int j1Index, j2Index, j3Index;
+private:
+    float discriminator_;
+    int type_;
+    unsigned int j1Index_, j2Index_, j3Index_;
 
+public:
     TopObjLite() {}
-    TopObjLite(const TopObject& top) : p4(top.p()), discriminator(top.getDiscriminator()), type(top.getType()), j1Index(-1), j2Index(-1), j3Index(-1)
+    TopObjLite(const TopObject& top) : reco::LeafCandidate(), discriminator_(top.getDiscriminator()), type_(top.getType()), j1Index_(-1), j2Index_(-1), j3Index_(-1)
     {
+        //Initialize the LeafCandidate's p4 with the top p4
+        construct(0, top.p().Pt(), top.p().Eta(), top.p().Phi(), top.p().M(), reco::LeafCandidate::Point(0, 0, 0), 0, 0);
+
+        //Get the constituents of the top and set the index pointing to each jet
         const auto& constituents = top.getConstituents();
         const int NCONST = constituents.size();
-        if(NCONST >= 1) j1Index = constituents[0]->getIndex();
-        if(NCONST >= 2) j2Index = constituents[1]->getIndex();
-        if(NCONST >= 3) j3Index = constituents[2]->getIndex();
+        if(NCONST >= 1) j1Index_ = constituents[0]->getIndex();
+        if(NCONST >= 2) j2Index_ = constituents[1]->getIndex();
+        if(NCONST >= 3) j3Index_ = constituents[2]->getIndex();
     }
+
+    //getters for the internal variables
+    float getDiscriminator() const { return discriminator_; }
+    int getType() const { return type_; }
+    unsigned int getJ1Idx() const { return j1Index_; }
+    unsigned int getJ2Idx() const { return j2Index_; }
+    unsigned int getJ3Idx() const { return j3Index_; }
 };
