065c9189bf8bf11ba8411254f0cf97e1e27ab7d7
Nathaniel <pastika@cern.ch>
2019-01-09 11:53:42 -0600

    Update CMSSW version for travis

--- .travis/runTests.sh
+++ .travis/runTests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CMSSW_VERSION=CMSSW_10_2_9
+
 #stop upon failed command
 set -ex
 
@@ -9,8 +11,8 @@ export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 export SCRAM_ARCH=slc6_amd64_gcc630
 export CMSSW_GIT_REFERENCE=/cvmfs/cms.cern.ch/cmssw.git/                       
 source $VO_CMS_SW_DIR/cmsset_default.sh 
-scramv1 project CMSSW CMSSW_9_4_11
-cd CMSSW_9_4_11/src/
+scramv1 project CMSSW $CMSSW_VERSION
+cd $CMSSW_VERSION/src/
 #suppress huge printout from "cmsenv"
 set +x
 echo "============================"
