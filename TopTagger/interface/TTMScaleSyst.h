a1d24e09fb585caab6e63de82c762e591cfb130d
Nathaniel <pastika@cern.ch>
2019-04-17 14:38:30 -0500

    Finish change to add SF anf systematic loading module

--- TopTagger/interface/TTMScaleSyst.h
+++ TopTagger/interface/TTMScaleSyst.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <memory>
 #include <map>
+#include <functional>
 
 class TopTaggerResults;
 class TH1;
@@ -22,10 +23,16 @@ class TTMScaleSyst : public TTModule
 private:
     /// Store the type of TopObject to operate on
     TopObject::Type topType_;
-    /// A histogram containing the scale factor information
-    std::unique_ptr<TH1> scaleFactorHist_;
+    /// Store the type of TopObject in string format
+    std::string topFlavor_;
+    /// Set of scale factor histograms
+    std::map<std::string, std::unique_ptr<TH1>> scaleFactorHists_;
     /// Set of histograms containing the systeamtic uncertainties 
     std::map<std::string, std::unique_ptr<TH1>> systematicHists_;
+    /// Map containing the variable to use for each SF and systematic histogram
+    std::map<std::string, std::function<float(const TopObject&)>> variables_;
+
+    std::function<float(const TopObject&)> parseVariable(const std::string&);
 
 public:
     void getParameters(const cfg::CfgDocument*, const std::string&);
--- TopTagger/src/TTMScaleSyst.cpp
+++ TopTagger/src/TTMScaleSyst.cpp
@@ -14,12 +14,21 @@ void TTMScaleSyst::getParameters(const cfg::CfgDocument* cfgDoc, const std::stri
     //cfg::Context commonCxt("Common");
     cfg::Context localCxt(localContextName);
 
-    std::string topFlavor           = cfgDoc->get("topFlavor",        localCxt, "NONE");
-    std::string inputFileName       = cfgDoc->get("inputFile",        localCxt, "");
-    std::string scaleFactorHistName = cfgDoc->get("scaleFactorHist",  localCxt, "");
-    std::string scaleFactorHistVar  = cfgDoc->get("scaleFactorVar",   localCxt, "");
+    topFlavor_                 = cfgDoc->get("topFlavor",        localCxt, "NONE");
+    std::string inputFileName  = cfgDoc->get("inputFile",        localCxt, "");
 
-    //Get histogram names and variables for systematic evaluation
+    //Select top type
+    if(     topFlavor_.compare("MERGED_TOP") == 0)       topType_ = TopObject::MERGED_TOP;
+    else if(topFlavor_.compare("SEMIMERGEDWB_TOP") == 0) topType_ = TopObject::SEMIMERGEDWB_TOP;
+    else if(topFlavor_.compare("RESOLVED_TOP") == 0)     topType_ = TopObject::RESOLVED_TOP;
+    else if(topFlavor_.compare("MERGED_W") == 0)         topType_ = TopObject::MERGED_W;
+    else if(topFlavor_.compare("SEMIMERGEDQB_TOP") == 0) topType_ = TopObject::SEMIMERGEDQB_TOP;
+    else
+    {
+        THROW_TTEXCEPTION("Top flavor \"" + topFlavor_ + "\" is not supported!");
+    }
+
+    //Get histogram names and variables for scale factor and systematic evaluation
     std::vector<std::pair<std::string, std::string>> systematicNames;
 
     //Get the necessary and avaliable histograms from the input root file
@@ -31,18 +40,47 @@ void TTMScaleSyst::getParameters(const cfg::CfgDocument* cfgDoc, const std::stri
         THROW_TTEXCEPTION("File \"" + inputFileName + "\" is not valid");
     }
 
-    //Get scale factor histogram 
-    scaleFactorHist_.reset(static_cast<TH1*>(file->Get(scaleFactorHistName.c_str())));
-
-    //Check that the histogram pointer is valid
-    if(!scaleFactorHist_.get())
-    {
-        THROW_TTEXCEPTION("Scale factor histogram  \"" + scaleFactorHistName + "\" is not valid");
-    }
-
-    //Get systematic histograms
+    //Get scale factor histograms 
     int iVar = 0;
     bool keepLooping;
+    do
+    {
+        keepLooping = false;
+
+        //Get variable name
+        std::string sfHist =  cfgDoc->get("sfHist", iVar, localCxt, "");
+        std::string sfVar  =  cfgDoc->get("sftVar",  iVar, localCxt, "");
+            
+        //Get syst factor histogram 
+        TH1* h_temp = static_cast<TH1*>(file->Get(sfHist.c_str()));
+        
+        //Check that the histogram pointer is valid
+        if(!h_temp)
+        {
+            THROW_TTEXCEPTION("Scale factor histogram  \"" + sfHist + "\" is not valid");
+        }
+        
+        //if it is a non empty string save in vector
+        if(sfHist.size() > 0 && sfVar.size() > 0)
+        {
+            keepLooping = true;
+
+            scaleFactorHists_.emplace(sfHist, h_temp);
+        }
+        else if((sfHist.size() == 0) ^ (sfVar.size() == 0)) //^ here is to catch cases where one, but not both, variables are undefined
+        {
+            THROW_TTEXCEPTION("Scale factor histogram  \"" + sfHist + "\" with variable \"" + sfVar + "\" is not valid");
+        }
+
+        //Set the variable assigned to the histogram
+        variables_[sfHist] = parseVariable(sfVar);
+
+        ++iVar;
+    }
+    while(keepLooping);
+
+    //Get systematic histograms
+    iVar = 0;
     do
     {
         keepLooping = false;
@@ -71,6 +109,10 @@ void TTMScaleSyst::getParameters(const cfg::CfgDocument* cfgDoc, const std::stri
         {
             THROW_TTEXCEPTION("Systematic histogram  \"" + systHist + "\" with variable \"" + systVar + "\" is not valid");
         }
+
+        //Set the variable assigned to the histogram
+        variables_[systHist] = parseVariable(systVar);
+
         ++iVar;
     }
     while(keepLooping);
@@ -82,17 +124,61 @@ void TTMScaleSyst::run(TopTaggerResults& ttResults)
 {
     //Get final tops of the desired type    
     //Check that tops of this type actually exist
-    auto& topsByType = ttResults.getTopsByType();
-    if(topsByType.find(topType_) != topsByType.end())
-    {
-        //Nothing to do for this event
-        return;
-    }
-    std::vector<TopObject*>& tops = ttResults.getTopsByType()[topType_];
+    auto& topCands = ttResults.getTopCandidates();
 
     //Set scale factor for each top
-    for(TopObject* top : tops)
+    for(TopObject& topCand : topCands)
     {
-        
+        if(topCand.getType() == topType_)
+        {
+            if(topCand.getBestGenTopMatch())
+            {
+                const auto& sfHist = scaleFactorHists_["SF_SIG_" + topFlavor_];
+                const auto& varGetter = variables_["SF_SIG_" + topFlavor_];
+                double scaleFactor = sfHist->GetBinContent(sfHist->FindBin(varGetter(topCand)));
+                topCand.setMCScaleFactor(scaleFactor);
+
+                for(auto& syst : systematicHists_)
+                {
+                    if(syst.first.find("SYST_SIG") != std::string::npos)
+                    {
+                        const auto& systHist = syst.second;
+                        const auto& varGetter = variables_[syst.first];
+                        double systVal = systHist->GetBinContent(systHist->FindBin(varGetter(topCand)));
+                        topCand.setSystematicUncertainty(syst.first, systVal);
+                    }
+                }
+            }
+            else
+            {
+                auto& sfHist = scaleFactorHists_["SF_BG_" + topFlavor_];
+                auto& varGetter = variables_["SF_BG_" + topFlavor_];
+                double scaleFactor = sfHist->GetBinContent(sfHist->FindBin(varGetter(topCand)));
+                topCand.setMCScaleFactor(scaleFactor);
+
+                for(auto& syst : systematicHists_)
+                {
+                    if(syst.first.find("SYST_BG") != std::string::npos)
+                    {
+                        const auto& systHist = syst.second;
+                        const auto& varGetter = variables_[syst.first];
+                        double systVal = systHist->GetBinContent(systHist->FindBin(varGetter(topCand)));
+                        topCand.setSystematicUncertainty(syst.first, systVal);
+                    }
+                }
+            }
+        }
+    }
+}
+
+std::function<float(const TopObject&)> TTMScaleSyst::parseVariable(const std::string& var)
+{
+    if(     var.compare("pt") == 0)   return [](const TopObject& topCand) { return topCand.p().Pt(); } ;
+    else if(var.compare("eta") == 0)  return [](const TopObject& topCand) { return topCand.p().Eta(); } ;
+    else if(var.compare("phi") == 0)  return [](const TopObject& topCand) { return topCand.p().Phi(); } ;
+    else if(var.compare("mass") == 0) return [](const TopObject& topCand) { return topCand.p().M(); } ;
+    else 
+    {
+        THROW_TTEXCEPTION("Unknown TopObject variable: \"" + var + "\"!");
     }
 }
